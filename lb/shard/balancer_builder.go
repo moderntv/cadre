de2828cf5bc3db6baba05550da7d783c522357fd
Svätopluk Hanzel <svatoplukhanzel@pm.me>
2022-03-02 02:24:12 +0100

    feat: named balancer builder

--- lb/shard/balancer_builder.go
+++ lb/shard/balancer_builder.go
@@ -16,3 +16,13 @@ func NewBuilder() (balancer.Builder, error) {
 
 	return base.NewBalancerBuilder(Name, pickerBuilder, base.Config{HealthCheck: true}), nil
 }
+
+func NewNamedBuilder(name string) (balancer.Builder, error) {
+	options := defaultBuilderOptions()
+	pickerBuilder, err := newPickerBuilder(options)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create picker builder: %v", err)
+	}
+
+	return base.NewBalancerBuilder(name, pickerBuilder, base.Config{HealthCheck: true}), nil
+}
