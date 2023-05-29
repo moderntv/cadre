9a12a252a3e089176d66558fdb537e797b477d0f
Martin Vaško <matovidlo2@gmail.com>
2023-05-29 13:43:42 +0200

    fix(resolver): Add more logging info about service incorrectly configured
    Also provide error that occured.

--- registry/resolver.go
+++ registry/resolver.go
@@ -92,7 +92,7 @@ func (rr *registryResolver) updateAddressesFromRegistry() {
 		Addresses: addrs,
 	})
 	if err != nil {
-		logger.Errorln("[RESOLVER] service connection update failed")
+		logger.Errorf("[RESOLVER] service connection update failed from registry: `%v`, err: %v", rr.service.Name(), err)
 		return
 	}
 }
