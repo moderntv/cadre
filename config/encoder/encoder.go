39d6a42d68f559b2562c3ec4e22eae05eeb81db8
Stanislav Zeman <stanislav.zeman@moderntv.eu>
2024-08-05 14:25:46 +0200

    fix: inamedparam linter

--- config/encoder/encoder.go
+++ config/encoder/encoder.go
@@ -2,6 +2,6 @@ package encoder
 
 // Encoder is responsible for encoding/decoding values read from source.
 type Encoder interface {
-	Encode(interface{}) ([]byte, error)
-	Decode([]byte, interface{}) error
+	Encode(data any) ([]byte, error)
+	Decode(data []byte, destination any) error
 }
